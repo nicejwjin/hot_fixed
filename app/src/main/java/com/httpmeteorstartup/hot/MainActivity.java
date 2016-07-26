e3b19c6e8ce24fec21e3a2681006b9b3bd13b08c
jwjin <jwjin@me.com>
2016-07-26 15:36:46 +0900

    scroll layout

--- app/src/main/java/com/httpmeteorstartup/hot/MainActivity.java
+++ app/src/main/java/com/httpmeteorstartup/hot/MainActivity.java
@@ -1,40 +1,15 @@
 package com.httpmeteorstartup.hot;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
+         setContentView(R.layout.activity_main);
 
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT);
-
-        LinearLayout baseLayout = new LinearLayout(this);
-        baseLayout.setOrientation(LinearLayout.VERTICAL);
-        baseLayout.setBackgroundColor(Color.rgb(0, 255, 0));
-        setContentView(baseLayout, params);
-
-        Button btn = new Button(this);
-        btn.setText("¹öÆ°ÀÔ´Ï´Ù");
-        btn.setBackgroundColor(Color.MAGENTA);
-        baseLayout.addView(btn);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
-                Toast.makeText(getApplicationContext(), "ÄÚµå·Î »ý¼ºÇÑ ¹öÆ°ÀÔ´Ï´Ù", Toast.LENGTH_SHORT).show();
-
-            }
-        });
 
     }
 }
