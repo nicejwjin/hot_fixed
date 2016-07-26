14d5059a09395b2f189005cd759008c6bcb83c6f
jwjin <jwjin@me.com>
2016-07-26 16:27:31 +0900

    picture step 1

--- app/src/main/java/com/httpmeteorstartup/hot/MainActivity.java
+++ app/src/main/java/com/httpmeteorstartup/hot/MainActivity.java
@@ -1,10 +1,7 @@
 package com.httpmeteorstartup.hot;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -12,17 +9,6 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_main);
-
-        Button btnNewActivity = (Button) findViewById(R.id.btnNewActivity);
-        btnNewActivity.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,
-                        SecondActivity.class);
-                startActivity(intent);
-            }
-        }
-
-        );
 
     }
 }
